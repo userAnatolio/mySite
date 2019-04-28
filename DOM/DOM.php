5a6844fb94493ee682f94ade79731d7395668990
Tolik <89778964651@mail.ru>
2019-04-28 21:08:12 +0300

    дополнил

--- DOM/DOM.php
+++ DOM/DOM.php
@@ -8,72 +8,70 @@
 	</head>
 	<body>	
 	
-		<div id="net">
-			<div class="center">
-				<a href="https://vk.com/public161733877" target="_blank"><img src="img/vk2.png" width="25px"></a>
-				<a href="http://whatsap.me/pixel" target="_blank"><img src="img/whatsapp2.png" width="25px"></a>
-				<a href="https://www.instagram.com/toly87/" target="_blank"><img src="img/instagram2.png" width="25px"></a>
-				<a href="https://www.facebook.com/anatol.anatolio" target="_blank"><img src="img/facebook2.png" width="27px"></a>
-				<a href="https://github.com/Anatolio87" target="_blank"><img src="img/github2.png" width="25px"></a>
-				<a href="https://habrahabr.ru/users/tolik-programist/" target="_blank"><img src="img/habrahabr2.png" width="25px"></a>
-			</div>
-		</div>
-	
 		<div id="wrapper">
-			<header>
-				<div class="center">
-					<div id="logo">
-						<a href="/mySite/inner.php">Pixel.ru</a>
-					</div>
-					<div id="slogan"></div>
-						<nav>
-							<?php
-								if(file_exists('menu/menu.php'))
-								{
-									include 'menu/menu.php';
-								}
-								else echo 'Файл не существует';
-							?>
-						</nav>
-					<div id="logotip"><img src=""></div>
+			<div class="center">
+				<div id="net">
+						<a href="https://vk.com/public161733877" target="_blank"><img src="img/vk2.png" width="25px"></a>
+						<a href="http://whatsap.me/pixel" target="_blank"><img src="img/whatsapp2.png" width="25px"></a>
+						<a href="https://www.instagram.com/toly87/" target="_blank"><img src="img/instagram2.png" width="25px"></a>
+						<a href="https://www.facebook.com/anatol.anatolio" target="_blank"><img src="img/facebook2.png" width="27px"></a>
+						<a href="https://github.com/Anatolio87" target="_blank"><img src="img/github2.png" width="25px"></a>
+						<a href="https://habrahabr.ru/users/tolik-programist/" target="_blank"><img src="img/habrahabr2.png" width="25px"></a>
 				</div>
-			</header>
+				<header>
+						<div id="logo">
+							<a href="/mySite/inner.php">Pixel.ru</a>
+						</div>
+						<div id="slogan"></div>
+							<nav>
+								<?php
+									if(file_exists('menu/menu.php'))
+									{
+										include 'menu/menu.php';
+									}
+									else echo 'Файл не существует';
+								?>
+							</nav>
+							
+						<div id="logotip"><img src=""></div>
+						
+				</header>
+					<main>
+						<div id="content">
+								<?php // Альтернатива AJAX использовать include для загрузки содержимого подключаемыми страницами.
+									if($_GET['page'] = 'start')
+										{
+											if(file_exists('content/content.php'))
+											{
+												include 'content/content.php';
+											}
+											else echo 'Файл не существует';
+										}
+									else 
+										{
+											if(file_exists('content/' . $_GET['page']))
+											{
+												include 'content/'. $_GET['page'];
+											}
+											else echo 'Файл не существует';
+										}
+										
+								?>
+						</div>
+					</main>	
+				
+				<script src="JS/ajaxMenu.js"></script>
 			
-			<script src="JS/ajaxMenu.js"></script>
-			<main>
-				<div class="center">
-					<div id="content">
-						<?php // Альтернатива AJAX использовать include для загрузки содержимого подключаемыми страницами.
-							if($_GET['page'] = 'start')
-								{
-									if(file_exists('content/content.php'))
-									{
-										include 'content/content.php';
-									}
-									else echo 'Файл не существует';
-								}
-							else 
-								{
-									if(file_exists('content/' . $_GET['page']))
-									{
-										include 'content/'. $_GET['page'];
-									}
-									else echo 'Файл не существует';
-								}
-								
-						?>
-					</div>
-				</div>
-			</main>
-			<footer>
-				<?php
-					if(file_exists('footer/footer.php'))
-					{
-						include 'footer/footer.php';
-					}
-					else echo 'Файл не существует';
-				?>
-			</footer>
+				<footer>
+					<?php
+						if(file_exists('footer/footer.php'))
+						{
+							include 'footer/footer.php';
+						}
+						else echo 'Файл не существует';
+					?>
+				</footer>
+			</div>
 		</div>
 	</body>
 </html>
--- footer/footer.php
+++ footer/footer.php
@@ -1,10 +1,8 @@
-<footer>
-<div class="center">
+
     <div id="footer">
-    <div class="footer-block"></div>
-    <div class="footer-block"></div>
-    <div class="footer-block"></div>
+		<div class="footer-block"></div>
+		<div class="footer-block block-footer"></div>
+		<div class="footer-block"></div>
     </div>
-</div>
-</footer>
-<div id="copy"></div>
+	
+	<div id="copy"></div>
